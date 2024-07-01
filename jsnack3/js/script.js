befc2c9fa429960bccd7be94e401cf6d0b04e01d
EndriuK <endrit.kaskija@gmail.com>
2024-07-01 17:57:40 +0200

    [UPD] creo for

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -3,3 +3,9 @@ let numero = prompt("Inserisci un numero di 4 cifre:");
 
 // DIVIDO IL NUMERO CON SPLIT
 let cifre = numero.split("");
+
+// CALCOLO LA SOMMA DELLE CIFRE
+let somma = 0;
+for (let i = 0; i < cifre.length; i++) {
+  somma += parseInt(cifre[i]);
+}
