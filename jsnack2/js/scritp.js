3e47d90cac1829dd774043adaea0cb96fde81cab
EndriuK <endrit.kaskija@gmail.com>
2024-07-01 16:43:19 +0200

    [UPD] verifica numero dispari

--- jsnack2/js/scritp.js
+++ jsnack2/js/scritp.js
@@ -4,4 +4,8 @@ let arrayDispari = [];
 // CREAZIONE DEL PROMPT PER L'UTENTE
 for (let i = 0; i < 6; i++) {
   let numero = parseInt(prompt("Inserisci un numero:"));
+
+  //VRIFICA SE IL NUMERO INSERITO E' DISPARI
+  if (numero % 2 !== 0) {
+  }
 }
