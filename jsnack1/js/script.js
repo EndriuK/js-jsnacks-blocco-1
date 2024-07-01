f58d29ad7942f817420b42ac417c40f66b6e1d06
EndriuK <endrit.kaskija@gmail.com>
2024-07-01 15:45:08 +0200

    [UPD] fine primo jsnack

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -9,10 +9,19 @@ btn.addEventListener("click", function () {
   // RECUPERO IL VALORE DEL NOME DA INSERIRE
   let userName = document.getElementById("nomi").value;
 
+  // DEFINISCO UNA VARIABILE FLAG PER IL CONTROLLO DEL NOME
+  let controllo = false;
+
   for (let i = 0; i < nomi.length; i++) {
     // CONTROLLO SE IL NOME INSERITO E' PRESENTE NELL'ARRAY
     if (userName == nomi[i]) {
-      console.log("ACCESSO AUTORIZZATO");
+      controllo = true;
     }
+  }
+
+  if (controllo) {
+    console.log("il nome inserito e' presente nella lista");
+  } else {
+    console.log("il nome inserito non e' presente nella lista");
   }
 });
