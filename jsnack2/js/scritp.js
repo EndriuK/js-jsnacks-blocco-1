6feb6c2fdd342be52f3c323bb3030438ea41818d
EndriuK <endrit.kaskija@gmail.com>
2024-07-01 16:40:19 +0200

    [UPD] creazione del prompt e for

---
+++ jsnack2/js/scritp.js
@@ -0,0 +1,7 @@
+// CREAZIONE DELL'ARRAY VUOTO
+let arrayDispari = [];
+
+// CREAZIONE DEL PROMPT PER L'UTENTE
+for (let i = 0; i < 6; i++) {
+  let numero = parseInt(prompt("Inserisci un numero:"));
+}
