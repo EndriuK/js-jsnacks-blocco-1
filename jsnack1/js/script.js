e1feb071cdd1122e6d941070f79c5e60f84d6afc
EndriuK <endrit.kaskija@gmail.com>
2024-07-01 15:08:04 +0200

    [UPD] recupero btn più aggiunta evento

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -1,2 +1,11 @@
 // DEFINISCO L'ARRAY DI NOMI SU QUALE ANDARE AD EFFETURE IL CONTROLLO
 let name = ["pippo", "paperino", "pluto"];
+
+// RECUPERO IL PULSANTE DAL DOM
+const btn = document.getElementById("check");
+
+// AGGIUNGO L'EVENTO CLICK AL PULSANTE
+btn.addEventListener("click", function () {
+  // RECUPERO IL VALORE DEL NOME DA INSERIRE
+  let userName = document.getElementById("name").value;
+});
