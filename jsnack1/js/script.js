59d4d3451027eab3e07d931d507bbd5049787186
EndriuK <endrit.kaskija@gmail.com>
2024-07-01 15:21:06 +0200

    [UPD] creo ciclo for

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -1,5 +1,5 @@
 // DEFINISCO L'ARRAY DI NOMI SU QUALE ANDARE AD EFFETURE IL CONTROLLO
-let name = ["pippo", "paperino", "pluto"];
+let nomi = ["pippo", "paperino", "pluto"];
 
 // RECUPERO IL PULSANTE DAL DOM
 const btn = document.getElementById("check");
@@ -7,5 +7,12 @@ const btn = document.getElementById("check");
 // AGGIUNGO L'EVENTO CLICK AL PULSANTE
 btn.addEventListener("click", function () {
   // RECUPERO IL VALORE DEL NOME DA INSERIRE
-  let userName = document.getElementById("name").value;
+  let userName = document.getElementById("nomi").value;
+
+  for (let i = 0; i < nomi.length; i++) {
+    // CONTROLLO SE IL NOME INSERITO E' PRESENTE NELL'ARRAY
+    if (userName == nomi[i]) {
+      console.log("ACCESSO AUTORIZZATO");
+    }
+  }
 });
