e435a7ab5735a3ef52f15018c4ba9d3f18308cda
EndriuK <endrit.kaskija@gmail.com>
2024-07-01 16:49:11 +0200

    [UPD] fine ciclo numeri

--- jsnack2/js/scritp.js
+++ jsnack2/js/scritp.js
@@ -7,5 +7,10 @@ for (let i = 0; i < 6; i++) {
 
   //VRIFICA SE IL NUMERO INSERITO E' DISPARI
   if (numero % 2 !== 0) {
+    // INSERISCO IL NUMERO NELL'ARRAY SE E' DISPARI
+    arrayDispari.push(numero);
+    console.log("Numero dispari aggiunto: " + numero);
+  } else {
+    console.log("Il numero è dispari");
   }
 }
